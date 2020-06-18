12854f6d305a98a2fab805b7bea748d7466fde20
Lilly Oh <ohlilly@gmail.com>
2020-06-18 13:09:03 -0400

    fixing missing db and seed file data

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,20 @@
 const User = require('./user')
+const Product = require('./product')
+const Order = require('./order')
+const ProductOrder = require('./productorder')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Order.belongsTo(User)
+User.hasMany(Order)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Order.hasMany(ProductOrder)
+ProductOrder.belongsTo(Order)
+
+Order.belongsToMany(Product, {through: ProductOrder})
+Product.belongsToMany(Order, {through: ProductOrder})
+
 module.exports = {
-  User
+  User,
+  Product,
+  Order,
+  ProductOrder
 }
---
+++ server/db/models/productorder.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const ProductOrder = db.define('productorder', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+    purchasePrice: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  }
+})
+
+module.exports = ProductOrder
